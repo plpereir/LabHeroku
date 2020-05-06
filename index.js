a9dd3a7d91dcdb7e5a3390647fd9bacf0b39ecae
plpereir <pl.silvapereira@gmail.com>
2020-05-06 11:10:46 -0300

    change port server

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ router.get('/sitemap',function(req,res){
 //add the router
 app.use('/', router);
 app.use(express.static('public'));
-app.listen(process.env.port || 3000);
+app.listen(process.env.port || 8080);
 
-console.log('Running at Port 3000');
+console.log('Running at Port 8080');
