33ad0012afc2232b2f06e86d1ec0eae7ce112d27
plpereir <pl.silvapereira@gmail.com>
2020-05-07 00:04:19 -0300

    Completed code review

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
-var express = require('express')
-var path = require('path')
-var serveStatic = require('serve-static')
+var express = require('express');
+var app = express();
 
-var app = express()
-app.use(serveStatic(path.join(__dirname, 'dist')))
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
 
-var port = process.env.PORT || 8000
-app.listen(port)
-console.log('server started ' + port)
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+    // ejs render automatically looks in the views folder
+    res.render('index');
+});
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
+/*
+- grabbing all the packages that we need
+- setting the view engine to EJS (more info)
+- using express to serve static assets from the public folder
+- creating a home page route to deliver a web page (more info)
+- setting a dynamic port
+- making the application listen on that port
+*/
